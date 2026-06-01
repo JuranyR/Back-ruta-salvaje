9fabab65a03fdaa23a9b4bda268d2d88e5b91367
Cristian Ariza <arizacris2431@gmail.com>
2026-06-01 16:27:28 -0500

    pom

--- src/main/java/com/proyectoFinal/rutaSalvaje/config/SecurityConfig.java
+++ src/main/java/com/proyectoFinal/rutaSalvaje/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -25,6 +26,16 @@ public class SecurityConfig {
     public SecurityConfig(JwtFilter jwtFilter, AuthenticationProvider authenticationProvider) {
         this.jwtFilter = jwtFilter;
         this.authenticationProvider = authenticationProvider;
+    }
+
+    // IGNORAR TOTALMENTE SWAGGER A NIVEL WEB (Solución radical para producción)
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html"
+        );
     }
 
     @Bean
